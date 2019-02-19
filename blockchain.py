4864a40994f6343b0a0c1f483e36571c32c5f290
Rohan Mundhey <mundheyrohan@gmail.com>
2019-02-19 14:11:53 +0530

    day 1

--- blockchain.py
+++ blockchain.py
@@ -1,4 +1,22 @@
 #Blockchain ASIC Resistance
+#
+#Previous POW ASIC Resistant Algorithms
+#SHA256   for Bitcoin
+#Scrypt   for Litecoin
+#Ethash   for Etherium
+#X11      for Dash
+#X16R     for Ravencoin
+#Equihash for Zcash
+#
+#
+
+
+
+#Approach
+#approach is to use a sequence of hashing algorithms where the output of one
+#becomes the input to the next.
+#chaining more algorithms together adds difficulty in constructing an ASIC
+#
 
 
 def Algo1():
