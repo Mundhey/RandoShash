1705dae8f8311da12ec25bc8d0cc36d0cce2776b
Rohan Mundhey <mundheyrohan@gmail.com>
2019-06-03 16:37:15 +0530

    Code Formatting

--- blockchain.py
+++ blockchain.py
@@ -7,26 +7,10 @@ import random
 import requests
 from flask import Flask, jsonify, request
 
-"""
-fun_list=[hashlib.sha256,hashlib.sha512,hashlib.md5]
-
-#Hash of Bitcoin block mined on 21/2/2019
-
-#xyz="00000000000000000002a0fa3eb60b3be73ae5715c459384ef5e4de6fdc3a1d7"
-
-
-#for _ in range(10):
-
-#    xyz_binary=str.encode(xyz)
-#    hash_object=random.choice(fun_list)(xyz_binary)
-#    print(hash_object.hexdigest())
-#    xyz=hash_object.hexdigest()
-
-"""
-
 
 class Blockchain:
     def __init__(self):
+
         self.current_transactions = []
         self.chain = []
         self.nodes = set()
@@ -35,11 +19,6 @@ class Blockchain:
         self.new_block(previous_hash='1', proof=100)
 
     def register_node(self, address):
-        """
-        Add a new node to the list of nodes
-
-        :param address: Address of node. Eg. 'http://192.168.0.5:5000'
-        """
 
         parsed_url = urlparse(address)
         if parsed_url.netloc:
@@ -51,12 +30,6 @@ class Blockchain:
             raise ValueError('Invalid URL')
 
     def valid_chain(self, chain):
-        """
-        Determine if a given blockchain is valid
-
-        :param chain: A blockchain
-        :return: True if valid, False if not
-        """
 
         last_block = chain[0]
         current_index = 1
@@ -81,12 +54,6 @@ class Blockchain:
         return True
 
     def resolve_conflicts(self):
-        """
-        This is our consensus algorithm, it resolves conflicts
-        by replacing our chain with the longest one in the network.
-
-        :return: True if our chain was replaced, False if not
-        """
 
         neighbours = self.nodes
         new_chain = None
@@ -115,13 +82,6 @@ class Blockchain:
         return False
 
     def new_block(self, proof, previous_hash):
-        """
-        Create a new Block in the Blockchain
-
-        :param proof: The proof given by the Proof of Work algorithm
-        :param previous_hash: Hash of previous Block
-        :return: New Block
-        """
 
         block = {
             'index': len(self.chain) + 1,
@@ -138,14 +98,7 @@ class Blockchain:
         return block
 
     def new_transaction(self, sender, recipient, amount):
-        """
-        Creates a new transaction to go into the next mined Block
 
-        :param sender: Address of the Sender
-        :param recipient: Address of the Recipient
-        :param amount: Amount
-        :return: The index of the Block that will hold this transaction
-        """
         self.current_transactions.append({
             'sender': sender,
             'recipient': recipient,
@@ -160,26 +113,13 @@ class Blockchain:
 
     @staticmethod
     def hash(block):
-        """
-        Creates a SHA-256 hash of a Block
-
-        :param block: Block
-        """
 
         # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
         block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
 
     def proof_of_work(self, last_block):
-        """
-        Simple Proof of Work Algorithm:
 
-         - Find a number p' such that hash(pp') contains leading 4 zeroes
-         - Where p is the previous proof, and p' is the new proof
-
-        :param last_block: <dict> last Block
-        :return: <int>
-        """
         start=time()
         last_proof = last_block['proof']
         last_hash = self.hash(last_block)
@@ -188,59 +128,24 @@ class Blockchain:
         while self.valid_proof(last_proof, proof, last_hash) is False:
             proof += 1
         end=time()
-        print(end-start)
+        print(end-start,"seconds")
         return proof
 
     @staticmethod
     def valid_proof(last_proof, proof, last_hash):
-        """
-        Validates the Proof
 
-        :param last_proof: <int> Previous Proof
-        :param proof: <int> Current Proof
-        :param last_hash: <str> The hash of the Previous Block
-        :return: <bool> True if correct, False if not.
-
-        """
-
-        """
-        Thesis Work
-        
-        fun_list=[hashlib.sha256,hashlib.sha512,hashlib.md5]
-
-        #Hash of Bitcoin block mined on 21/2/2019
-
-        #xyz="00000000000000000002a0fa3eb60b3be73ae5715c459384ef5e4de6fdc3a1d7"
-
-
-        for _ in range(10):
-
-            xyz_binary=str.encode(xyz)
-            hash_object=random.choice(fun_list)(xyz_binary)
-            print(hash_object.hexdigest())
-            xyz=hash_object.hexdigest()
-        
-        
-        
-        
-        """
         fun_list = [hashlib.sha256, hashlib.sha512, hashlib.md5,hashlib.sha3_512,hashlib.sha3_256]
         guess = f'{last_proof}{proof}{last_hash}'
         guess_copy = guess
 
         for xyz in range(random.randint(5,10)):
 
-
             abc=str.encode(guess_copy)
-
             hash_object=random.choice(fun_list)(abc)
-
             guess_copy=hash_object.hexdigest()
 
-
-
         guess_hash = hash_object.hexdigest()
-        return guess_hash[:3] == "000"
+        return guess_hash[:4] == "0000"
 
 
 # Instantiate the Node
